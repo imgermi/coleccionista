c4af72e13f883a71bf829cd3d6b8cf23ba524772
juangermano <juangermano@MacBook-Pro-de-Juan-2.local>
2016-12-14 11:38:36 -0300

    Add existing file

---
+++ js/functions.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+  $("#menu-1").click(function () {
+      $('html, body').animate({
+          scrollTop: $("#nosotros").offset().top
+      }, 500);
+    });
+});
+
+$(document).ready(function () {
+  $("#menu-2").click(function () {
+      $('html, body').animate({
+          scrollTop: $("#cafe").offset().top
+      }, 500);
+    });
+});
+
+$(document).ready(function () {
+  $("#menu-3").click(function () {
+      $('html, body').animate({
+          scrollTop: $("#testimonios").offset().top
+      }, 500);
+    });
+});
+
+$(document).ready(function () {
+  $("#menu-4").click(function () {
+      $('html, body').animate({
+          scrollTop: $("#ubicacion").offset().top
+      }, 500);
+    });
+});
+
+$(document).ready(function () {
+  $("#menu-5").click(function () {
+      $('html, body').animate({
+          scrollTop: $("#tour").offset().top
+      }, 500);
+    });
+});
+
+$(document).ready(function () {
+  $("#menu-6").click(function () {
+      $('html, body').animate({
+          scrollTop: $("#contacto").offset().top
+      }, 500);
+    });
+});
+
+$(document).ready(function () {
+  $(".main-logo").click(function () {
+      $('html, body').animate({
+          scrollTop: 0}, 500);
+    });
+});
